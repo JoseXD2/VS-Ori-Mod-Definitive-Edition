fff1326cb5e1eec18e5407819fa18f73fb78a835
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-09-13 14:07:39 -0300

    Update OptionsState.hx

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -95,6 +95,10 @@ class OptionsState extends MusicBeatState
 		changeSelection();
 		ClientPrefs.saveSettings();
 
+		#if android
+		addVirtualPad(UP_DOWN, A_B_C);
+                #end
+			
 		super.create();
 	}
 
@@ -124,6 +128,15 @@ class OptionsState extends MusicBeatState
 			MusicBeatState.switchState(new MainMenuState());
 		}
 
+		#if android
+		if (virtualPad.buttonC.justPressed) {
+			#if android
+			removeVirtualPad();
+			#end
+			MusicBeatState.switchState(new android.AndroidControlsSubState());
+		}
+		#end
+			
 		if (controls.ACCEPT) {
 			openSelectedSubstate(options[curSelected]);
 		}
