4b7509be4cd3cefb9468588975e1c9faf64b8f3f
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-09-15 08:48:05 -0300

    Shit gamejoit

--- source/gamejolt/GameJoltKeys.hx
+++
@@ -1,7 +0,0 @@
-package gamejolt;
-
-class GameJoltKeys
-{
-    public static var id:Int = 733942; // Put your game's ID here
-    public static var key:String = "fuck you"; // Put your game's private API key here
-}
