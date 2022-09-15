ddcbef064f90107cf4fd0448950c2965b0189bc5
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-09-15 09:18:29 -0300

    Update FlxVideo.hx

--- source/FlxVideo.hx
+++ source/FlxVideo.hx
@@ -1,4 +1,4 @@
-#if web
+#if desktop
 import openfl.net.NetConnection;
 import openfl.net.NetStream;
 import openfl.events.NetStatusEvent;
@@ -21,7 +21,7 @@ class FlxVideo extends FlxBasic {
 	public function new(name:String) {
 		super();
 
-		#if web
+		#if desktop
 		var player:Video = new Video();
 		player.x = 0;
 		player.y = 0;
