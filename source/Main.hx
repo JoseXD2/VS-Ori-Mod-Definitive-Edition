3280f6ec205290f16fe77ef3c6753601f1abb353
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-12-02 18:20:44 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -14,7 +14,7 @@ import lime.system.System;
 
 class Main extends Sprite
 {
-	var gameWidth: = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
+	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
